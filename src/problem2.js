4979d7d4e6f45fad49420e11ddd5e94b9ead9cb0
sdwoo <damjang67@naver.com>
2022-10-30 17:17:13 +0900

    feat(problem2): 암호 해독 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let duplicationCount = 1;
+  const regExp = /((.))\2*/g;
+  while (duplicationCount > 0) {
+    const regCripto = cryptogram.match(regExp);
+
+    if (!regCripto) {
+      cryptogram = '';
+      break;
+    }
+
+    cryptogram = regCripto.filter((char) => char.length === 1).join('');
+    duplicationCount = regCripto.filter((char) => char.length > 1).length;
+  }
+
+  return cryptogram ? cryptogram : '';
 }
+
+console.log(problem2('browoanoommnaon'));
+console.log(problem2('zyelleyz'));
 
 module.exports = problem2;
