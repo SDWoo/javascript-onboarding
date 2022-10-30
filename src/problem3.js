7ecf3cc0f6e74ba2e17f4d43f9d95f7e3a428dc1
sdwoo <damjang67@naver.com>
2022-10-30 17:25:32 +0900

    feat(problem3): 369 게임 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i < number + 1; i += 1) {
+    i.toString()
+      .split('')
+      .forEach((num) => {
+        if (num === '3' || num === '6' || num === '9') answer += 1;
+      });
+  }
+
   return answer;
 }
 
