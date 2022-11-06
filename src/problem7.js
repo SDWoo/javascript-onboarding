59b591fe5c0ee831ed8865aa8a62e3451ebf2970
sdwoo <damjang67@naver.com>
2022-11-06 15:50:52 +0900

    feat(problem7): 친구 추천 알고리즘 구현 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,42 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const answer = [];
+  const score = new Map();
+  const firstFriends = [];
+  const getFriend = (array, friend) => {
+    return array.filter((person) => person !== friend)[0];
+  };
+
+  friends.forEach((friend) => {
+    if (friend.includes(user)) {
+      friend = getFriend(friend, user);
+      firstFriends.push(friend);
+    }
+  });
+
+  firstFriends.forEach((friend) => {
+    for (let i = 0; i < friends.length; i++) {
+      const secondFriend = friends[i].includes(friend)
+        ? getFriend(friends[i], friend)
+        : '';
+
+      if (secondFriend.length > 0 && secondFriend !== user) {
+        score.set(secondFriend, score.get(secondFriend) + 10 || 10);
+      }
+    }
+  });
+
+  visitors.forEach((visitor) => {
+    if (!firstFriends.includes(visitor)) {
+      score.set(visitor, score.get(visitor) + 1 || 1);
+    }
+  });
+
+  return [...score.entries()]
+    .sort((a, b) => {
+      if (a[1] === b[1]) b[0] - a[0];
+      else b[1] - a[1];
+    })
+    .map((friend) => friend[0]);
 }
 
 module.exports = problem7;
